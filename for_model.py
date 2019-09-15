8e915963c6e0c1d311c13a4d31aec882d0f1ea87
kimura miyuki <hcp.miyuki@gmail.com>
2019-09-15 14:41:47 +0900

    [fix]モデルの読み込みをグローバル変数で読み込むように修正

--- for_model.py
+++ for_model.py
@@ -2,8 +2,7 @@ from keras.preprocessing.image import load_img, img_to_array
 from keras.models import load_model
 import argparse
 
-
-
+model=load_model('fer2013_mini_XCEPTION.110-0.65.hdf5', compile=False)
 
 def for_model(image_array):
     # X = []
@@ -13,7 +12,7 @@ def for_model(image_array):
     classes = ({0:'angry',1:'disgust',2:'fear',3:'happy',
         4:'sad',5:'surprise',6:'neutral'})
 
-    model=load_model('fer2013_mini_XCEPTION.110-0.65.hdf5', compile=False)
+
     pre = model.predict(image_array)[0]
     top_indices = pre.argsort()[-5:][::-1]
     result = [(classes[i] , pre[i]) for i in top_indices]
